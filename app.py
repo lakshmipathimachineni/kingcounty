a5265fb2d9306d507e33bfe4ebc62b2d21c52858
Ajay Naik <ajay.naik44@gmail.com>
2023-05-12 16:56:12 -0700

    Modify comma code

--- app.py
+++ app.py
@@ -18,10 +18,18 @@ def home():
 
 @app.route('/get_all_trip_details/<trip_ids>', methods=['GET'])
 def read_stops(trip_ids):
+    # Split the original string at each comma
+    split_values = trip_ids.split(',')
+
+    # Enclose each split value in single quotes
+    quoted_values = ["'{}'".format(value) for value in split_values]
+
+    # Join the quoted values using commas
+    result_string = ','.join(quoted_values)
     conn = sqlite3.connect('bus_transport.db')
     cursor = conn.cursor()
     cursor.execute(" select a.trip_id, b.stop_id,b.stop_code,b.stop_name,b.stop_desc,b.stop_lat,b.stop_lon, b.zone_id,b.stop_url,b.location_type,b.parent_station,b.stop_timezone\
-     from stop_times a , stops b where a.trip_id in ('"+trip_ids+"') and a.stop_id =  b.stop_id")
+     from stop_times a , stops b where a.trip_id in ("+result_string+") and a.stop_id =  b.stop_id")
     rows = cursor.fetchall()
     conn.close()
     return jsonify([StopOut(
